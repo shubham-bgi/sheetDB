296fa87cf089aa1c9a232145cd1480600c7c51e2
Shubham <shubhamaggarwal3011@gmail.com>
2023-04-30 21:16:53 +0530

    postgres not supported

--- src/utils.js
+++ src/utils.js
@@ -8,8 +8,8 @@ function getUrl(config) {
   switch (config.dbEngine) {
     case "mysql":
       return "jdbc:google:mysql://" + config.connectionName;
-    case "postgresql":
-      return "jdbc:google:postgresql://" + config.connectionName;
+    // case "postgresql":
+    //   return "jdbc:google:postgresql://" + config.connectionName;
     case "sqlserver":
       return "jdbc:google:sqlserver://" + config.connectionName;
   }
